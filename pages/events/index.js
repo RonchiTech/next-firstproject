89fce7d8da01d845e485edaad8864ec7b388675f
ronchitech <ronchibusiness@gmail.com>
2021-04-17 10:54:33 +0800

    fetch data from dummy database, added utility func

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head';
+import EventList from '../../components/events/EvenList'
+import { getAllEvents } from '../../dummy-data';
 const EventsPage = () => {
+  const allEvents = getAllEvents();
   return (
     <div>
       <Head>
         <title>Next Project | Events Page</title>
       </Head>
       <h1>Welcome to Events Page</h1>
+      <EventList items={allEvents}/>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
 import Head from 'next/head';
-import EvenList from '../components/events/EvenList'
-import { getFeaturedEvents } from '../dummy-data';
+import EvenList from '../components/events/EvenList';
+import { getFeaturedEvents } from '../helpers/api-utility';
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+export default function Home(props) {
+
   return (
     <div>
       <Head>
         <title>Next Project | Home Page</title>
       </Head>
       <h1>The Home Page</h1>
-      <EvenList items={featuredEvents} />
+      <EvenList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+ 
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 25,
+  };
 }
